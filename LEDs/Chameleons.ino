bdfeaa80bdefb311c34be55397ceb0a41f480e6b
JaeZ <J.Key.Noble@gmail.com>
2019-07-11 21:24:43 -0400

    Add files via upload

---
+++ LEDs/Chameleons.ino
@@ -0,0 +1,55 @@
+// CHAMELEONS - ALTERNATE CODE
+// This code will activate a blink all of LEDs when a photocell senses darkness.
+// Run the Serial monitor to check that the photoresistor is wired properly (Tools > Serial Monitor)
+
+void setup() {    // beginning of our setup() area.
+
+  Serial.begin(9600);  // start up the Serial Monitor.
+
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+
+}
+
+void loop() {     // beginning of our loop() area.
+
+
+  int sensorReading = analogRead(A0);   // read what the sensor is telling us
+  int i = analogRead(A0)/8; // divide it by 8 and call it "i", higher numbers will make it blink faster
+  Serial.println(sensorReading);        // show what the sensor is telling us in the Serial Monitor
+
+
+  if (sensorReading < 800) {           // If our sensor reads less than this number, then do the code between the {        }
+                                      // You can change this number between 0-1023 based on the readings you are getting. Check your serial monitor under the Tools menu.
+
+
+    digitalWrite(3, HIGH);
+    delay(i);
+    digitalWrite(3, LOW);
+
+    digitalWrite(5, HIGH);
+    delay(i);
+    digitalWrite(5, LOW);
+
+    digitalWrite(6, HIGH);
+    delay(i);
+    digitalWrite(6, LOW);
+
+    digitalWrite(9, HIGH);
+    delay(i);
+    digitalWrite(9, LOW);
+
+    digitalWrite(10, HIGH);
+    delay(i);
+    digitalWrite(10, LOW);
+
+    digitalWrite(11, HIGH);
+    delay(i);
+    digitalWrite(11, LOW);
+
+  }
+}
---
+++ LEDs/LED_Photo_Knob.ino
@@ -0,0 +1,53 @@
+// declare variables
+int blue = 13;
+int yellow = 11;
+
+int photoCell = 0;
+int knob = 0;
+
+int brightness = 0;
+int intensity = 0;
+
+void setup() {
+  Serial.begin(9600); // start serial monitor
+
+  // declare LED mode as output
+  pinMode(blue, OUTPUT);
+  pinMode(yellow, OUTPUT);
+}
+
+void loop() {
+  photoCell = analogRead(A0); //read from A0, which is photo sensor
+  knob = analogRead(A1); //read from A1, which is potienmeter
+  
+  int i = photoCell/8;  //devide the reading from photo sensor
+
+  //map the reading from photo sensor to the brightness of LED
+  brightness = map(photoCell, 650, 920, 0, 255); 
+
+  //map the reading from knob to the bright intensity of LED
+  intensity = map(knob, 1, 1024, 1, 255);
+
+  //print on serial monitor
+  Serial.print(photoCell);
+  Serial.print("\t");
+  Serial.println(brightness);
+
+  //write the reading from photosensor to LED
+  //analogWrite(yellow, 255-brightness); 
+
+  //write the reading from knob to LED
+  analogWrite(yellow, intensity);
+
+  //condition, when mapped reading greater than 180, blue light off
+  if (brightness >220) {
+    digitalWrite(blue, LOW);
+  } 
+  //otherwise, blink blue LED
+  else {
+    digitalWrite(blue, HIGH);
+    delay(i);
+    digitalWrite(blue, LOW);
+    delay(i);
+  }
+}
